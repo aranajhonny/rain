f724010b6f60da0ca162ce258a89dcd1d422c54d
aranajhonny <jhonnyjosearana@gmail.com>
2018-04-19 10:24:25 -0400

    init.

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 const { schema, imports, dependencies, environment, expressions, endpoints } = program;
 
+environment
+  .add('COUNTRY', 'The country code Ejm. us')
+  .add('ZIP', 'The ZIP code. Ejm. 94040')
+
+imports
+  .add('driver')
+  .add('twilio')
+
+dependencies
+  .add('messages', 'twilio:MessageCollection')
+  .add('locations', 'driver:LocationCollection')
+
 schema.type('Root')
- 
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const { root } = program.refs
-
+const { root, locations, messages } = program.refs;
+const { COUNTRY, ZIP } = process.env;
 export async function init() {
-  // Called when the program is run
+	return program.setTimer('check', 0, 50);
 }
-
-export async function update() {
-  // Called when the program is updated from a previous version
-}
-
 export async function timer({ key }) {
-  // Called every time a timer fires
-}
+	const currentDay = Date().getDate();
 
+	const weatherConditions = await locations
+		.byZipCode((country: COUNTRY), (zip: ZIP))
+		.list.items.query('{ dt dt_txt weather {main} }');
+
+	weatherConditions.map(item => {
+		const wHour = new Date(item.dt * 1000).getHours();
+		const wDay = new Date(item.dt * 1000).getDate();
+		const days = Wday - currentDay;
+		if (days === 1 && Whour === 21) {
+			console.log('El clima esta: ' + item.wheater.main);
+		}
+	});
+}
