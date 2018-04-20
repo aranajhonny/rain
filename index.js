51c6f87ee81215c102ae92a5aee1cda96e27e165
aranajhonny <jhonnyjosearana@gmail.com>
2018-04-20 15:35:36 -0400

    add twilio sms

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const { root, condition, messages } = program.refs;
 const { FROM, TO } = process.env;
 export async function init() {
-	return program.setTimer('check', 0, 10);
+  return program.setTimer('check', 0, 10);
 }
 export async function timer({ key }) {
-	const forecast = await condition.get();
-  console.log(forecast)
-	//if (condition === 'Rain') {
-		// await messages.sendSms({
-		// 	from: FROM,
-		// 	to: TO,
-		// 	body: `The forecast is ${condition}`,
-		// });
-	//}
+  if ((await condition.get()) === 'Rain') {
+    if (condition === 'Rain') {
+      await messages.sendSms({
+        from: FROM,
+        to: TO,
+        body: `The forecast is ${condition}`,
+      });
+    }
+  }
 }
